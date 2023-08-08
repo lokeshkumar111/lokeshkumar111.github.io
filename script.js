e57ad7e8602cb0bd77a36ebb2aaef845ca0b9e63
Lokesh <lokeshk.charan3@gmail.com>
2023-08-08 15:00:45 +0530

    some details remaining

---
+++ script.js
@@ -0,0 +1,43 @@
+
+window.addEventListener("scroll", () => {
+    const header = document.querySelector("header");
+    if (window.scrollY > 0) {
+      header.classList.add("sticky");
+    } else {
+      header.classList.remove("sticky");
+    }
+  });
+
+
+
+document.querySelector(".resumeDownload").addEventListener("click", function() {
+  const resumeFilePath = "./resume/Lokesh-Kumar-Resume.pdf";
+  downloadResume(resumeFilePath);
+});
+
+function downloadResume(fileUrl) {
+  const anchor = document.createElement("a");
+  anchor.style.display = "none";
+  anchor.href = fileUrl;
+  anchor.download = "lokesh-Kumar-Resume"; 
+  document.body.appendChild(anchor);
+  anchor.click();
+  document.body.removeChild(anchor);
+}
+
+
+// Integrate GitHub calendar
+GitHubCalendar(".calendar", "lokeshkumar111");
+
+// or enable responsive functionality:
+GitHubCalendar(".calendar", "lokeshkumar111", { responsive: true });
+
+// // Use a proxy if needed
+GitHubCalendar(".calendar", "lokeshkumar111", {
+  proxy(username) {
+    return fetch(`https://your-proxy.com/github?user=${username}`);
+  }
+}).then((r) => r.text());
+
+
+  
