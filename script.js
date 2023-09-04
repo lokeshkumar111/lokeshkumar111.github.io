fa68290adb3c17c38e7bc283bb9575a51042a1b0
lokeshkumar111 <lokeshk.charan3@gmail.com>
2023-09-04 15:00:25 +0530

    add slideshow

--- script.js
+++ script.js
@@ -25,6 +25,24 @@ function downloadResume(fileUrl) {
   document.body.removeChild(anchor);
 }
 
+// style for slideshow
+let slideIndex = 0;
+const slides = document.querySelectorAll('.slide');
+
+function showSlides() {
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = 'none';
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    slides[slideIndex - 1].style.display = 'block';
+    setTimeout(showSlides, 2000); 
+}
+
+showSlides();
+
 
 // Integrate GitHub calendar
 GitHubCalendar(".calendar", "lokeshkumar111");
@@ -38,6 +56,7 @@ GitHubCalendar(".calendar", "lokeshkumar111", {
     return fetch(`https://your-proxy.com/github?user=${username}`);
   }
 }).then((r) => r.text());
+
 
 
   
